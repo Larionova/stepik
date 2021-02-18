1200571647838e7b59dc953604fe3161fd3d701c
Larionova <helgal.ol70@gmail.com>
2021-02-18 21:07:53 +0200

    new file

--- test_parser.py
+++ test_parser.py
@@ -5,4 +5,4 @@ link = 'http://selenium1py.pythonanywhere.com/'
 def test_guest_should_see_login_link(driver):
     driver.get(link)
     driver.find_element(By.CSS_SELECTOR, '#login_link')
-    print("q")
+    print("r")
