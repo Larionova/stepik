65c8d703834cbf3c274e6b2ac0c4f26d473bae91
Olga Larionova <o.larionova@bossgs.com>
2021-02-18 16:30:37 +0200

    q

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--driver', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope='function')
+def driver(request):
+    driver_name = request.config.getoption('driver')
+    driver = None
+    if driver_name == 'chrome':
+        driver = webdriver.Chrome()
+    elif driver_name == 'firefox':
+        driver = webdriver.Firefox()
+    else:
+        raise pytest.UsageError('--driver should be chrome or firefox')
+    yield driver
+    driver.quit()
