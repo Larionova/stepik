677737c738179773ee6ac9c1161722932b87279d
Olga Larionova <o.larionova@bossgs.com>
2021-02-19 09:43:45 +0200

    delete print

--- test_parser.py
+++ test_parser.py
@@ -5,6 +5,3 @@ link = 'http://selenium1py.pythonanywhere.com/'
 def test_guest_should_see_login_link(driver):
     driver.get(link)
     driver.find_element(By.CSS_SELECTOR, '#login_link')
-    print("w")
-    print("q")
-    print("r")
